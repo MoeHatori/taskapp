4614e210ec01727d423e4e6c8362575e1e5d3718
moe.hatori <moe.hatori@jreast.co.jp>
2020-09-17 14:10:06 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by Chan Yama on 2020/09/17.
+//  Copyright © 2020 moe.hatori2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
